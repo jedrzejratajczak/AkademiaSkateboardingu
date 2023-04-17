36ea7db8e50076ce7ccc6439fe5495fe00b4a9a2
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2023-04-17 12:13:01 +0200

    Update costs

--- src/components/Costs.jsx
+++ src/components/Costs.jsx
@@ -7,13 +7,12 @@ const Costs = () => (
       <div className="text-black w-full md:w-max items-center md:left-12 lg:left-36 absolute text-2xl text-center flex flex-col gap-3 h-full justify-center">
         <p className="text-5xl">CENNIK</p>
         <p className="text-4xl mt-5">ZAJĘCIA GRUPOWE</p>
-        <p>* RAZ W TYGODNIU 2H</p>
-        <p className="underline">300 ZŁ / MSC</p>
-        <p>* 2 RAZY W TYGODNIU 2H</p>
-        <p className="underline">500 ZŁ / MSC</p>
+        <p>(max. 7 osób)</p>
+        <p className="underline">2-GODZINNE 100 ZŁ</p>
         <p className="text-4xl mt-5">ZAJĘCIA INDYWIDUALNE</p>
-        <p className="underline">* 100 ZŁ / H</p>
-        <p className="underline">* 150 ZŁ / 2H</p>
+        <p className="underline">1-GODZINNE 100 ZŁ</p>
+        <p className="text-4xl mt-5">WYPOŻYCZENIE SPRZĘTU</p>
+        <p className="underline">10 ZŁ</p>
       </div>
       <img
         className="h-[600px] w-screen object-cover"
